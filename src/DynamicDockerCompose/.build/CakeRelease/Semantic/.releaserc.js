1278f9b0e41a86a658115bf4777322203070cd4d
Sylvain <sylvain.signore@pixsys.be>
2024-09-04 16:36:02 +0200

    fix: test

--- src/DynamicDockerCompose/.build/CakeRelease/Semantic/.releaserc.js
+++ src/DynamicDockerCompose/.build/CakeRelease/Semantic/.releaserc.js
@@ -24,7 +24,7 @@ module.exports = {
         // the artifacts folder to NuGet
         [
             "@semantic-release/exec", {
-                "publishCmd": "Scripts/publishPackageToNuget.sh --token ${process.env.NUGET_TOKEN} --source ${process.env.PUBLISH_PACKAGE_TO_NUGET_SOURCE}"
+                "publishCmd": "./Scripts/publishPackageToNuget.sh --token ${process.env.NUGET_TOKEN} --source ${process.env.PUBLISH_PACKAGE_TO_NUGET_SOURCE}"
             }
         ]
     ],
